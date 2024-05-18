6ccbecb1b6153a83144ee25799ddc36d4f7cfcfc
ch34Tc0d3 <zoungranasteve64@gmail.com>
2024-05-18 22:02:31 +0200

    First step : help menu implement

--- client.py
+++ client.py
@@ -5,9 +5,9 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.send(b"Hello, world")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_server:
+    connection_server.connect((HOST, PORT))
+    connection_server.send(b"Hello, world")
+    data = connection_server.recv(1024)
 
 print(f"Received {data!r}")
--- server.py
+++ server.py
@@ -5,16 +5,35 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+help_menu =	{
+  "help": "Afficher la liste des commandes disponibles.",
+  "download": "Recuperation de fichiers de la victime vers le serveur.",
+  "upload": "Recuperation de fichiers du serveur vers la victime",
+  "shell": "ouvrir un shell (bash ou cmd) interactif.",
+  "ipconfig": "Obtenir la configuration reseau de la machine victime.",
+  "screenshot": "Prendre une capture d'ecran de la machine victime.",
+  "search": "Rechercher un fichier sur la machine victime",
+  "hashdump": "Recuperer la base SAM ou le fichier shadow de la machine."
+}
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_listening:
+    socket_listening.bind((HOST, PORT))
+    socket_listening.listen()
     print("[*] Listening on 65432...")
-    conn, addr = s.accept()
+    client_connection, client_address = socket_listening.accept()
     print("[+] Agent received !")
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.send(data)
+    with client_connection:
+        print(f"Connected by {client_address}")
+        user_input = input("rat > ")
+        while client_connection and user_input != "exit":
+            match user_input:
+                    case "help":
+                        for x, y in help_menu.items():
+                            print(f"\n{x} : {y}\n")
+
+            user_input = input("rat > ")
+
+            #data = client_connection.recv(1024)
+            #if not data:
+            #    break
+            #client_connection.send(data)
